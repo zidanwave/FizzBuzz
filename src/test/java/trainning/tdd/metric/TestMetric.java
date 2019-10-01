e7a518fb87fd6e33ee5b0945f2ed94d1a805ef40
zidanwave <3981833@qq.com>
2019-10-01 20:46:07 +0800

    补充了子任务拆解

--- src/test/java/trainning/tdd/metric/TestMetric.java
+++ src/test/java/trainning/tdd/metric/TestMetric.java
@@ -6,6 +6,19 @@ import org.testng.annotations.Test;
 /**
  * Created by chenzi on 2019/9/30.
  */
+
+//任务拆解：
+//        - 创建测试方法，英尺换算为英寸Foot.toInch()，期望输入1英尺，返回12英寸
+//        - 实现Foot.toInch() ，执行测试验证换算方法
+//        - 创建测试方法，码换算为英尺Yard.toFoot()，期望输入1码，返回3英尺
+//        - 实现Yard.toFoot()，执行测试验证换算方法
+//        - 创建测试方法，英寸换算为英尺Inch.toFoot()，期望输入1英寸，返回1/12英尺
+//        - 实现Inch.toFoot()，执行测试验证换算方法
+//        - 创建测试方法，英尺换算为码，期望输入1英尺，返回1/3码
+//        - 创建测试方法，英寸换算为英尺，期望输入1英尺，返回1/12英尺
+//        - 创建测试方法，英寸换算为码，期望输入1英尺，返回1/12/3码
+//        - 创建测试方法，码换算为英寸，期望输入1码，返回1*12*3英尺
+
 public class TestMetric {
 
     @Test
@@ -47,6 +60,8 @@ public class TestMetric {
     public void test_1_yard_to_inch() {
         Assert.assertEquals(new Yard(1).toInches(), 1.0*3*12);
     }
+
+
 
 
 }
